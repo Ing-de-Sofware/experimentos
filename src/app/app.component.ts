40435bfaa5a654a662c586f9a6577096d415b5d7
diegosoto <diegosotoquispe9876@gmail.com>
2025-06-04 12:33:16 -0500

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 import { UserTypeService } from './shared/services/user-type.service';
 import { UserType } from './shared/model/user-type.model';
 
@@ -10,23 +11,27 @@ import { UserType } from './shared/model/user-type.model';
 })
 export class AppComponent {
   title = 'frontend_version0_0';
-
-  get isAuthRoute(): boolean {
-    return [
-      '/login',
-      '/register',
-      '/forgot-password',
-      '/selectRole'
-    ].includes(this.router.url);
-  }
-
+  isAuthRoute: boolean = false;
   userType: UserType | null = null;
 
   constructor(
     private userTypeService: UserTypeService,
-    public router: Router
+    private router: Router
   ) {
     this.userTypeService.userType$.subscribe(type => this.userType = type);
+
+    // Escucha los cambios de ruta
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((event: any) => {
+        const currentPath = event.urlAfterRedirects.split('?')[0].replace(/\/$/, '');
+        this.isAuthRoute = [
+          '/login',
+          '/register',
+          '/forgot-password',
+          '/selectRole'
+        ].includes(currentPath);
+      });
   }
 
   showMedicalHistoryPage: boolean = false;
